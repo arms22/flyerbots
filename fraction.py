da05d8cccff90a3f8cd1e76e93f367885b5478ff
arms22 <arms22@gmail.com>
2019-03-26 00:26:03 +0900

    update fraction.py

--- fraction.py
+++ fraction.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
 from flyerbots.strategy import Strategy
 from flyerbots.indicator import *
-from math import floor, ceil
+from math import floor, ceil, sqrt
 from datetime import datetime, time
 
 suspension_period = [
     (time(18, 55), time(19, 55)), # JST 03:55-04:55 Bitflyerメンテナンスタイム
 ]
 
-def flooring(price, q):
-    return int(floor(price/q)*q)
-
-def ceiling(price, q):
-    return int(ceil(price/q)*q)
+def stdev(src):
+    average = sum(src) / len(src)
+    average_sq = sum(p**2 for p in src) / len(src)
+    var = average_sq - (average * average)
+    dev = sqrt(var)
+    return dev
 
 class fraction:
 
@@ -32,31 +33,38 @@ class fraction:
 
         if not coffee_break:
 
-            # 51円値幅で指値バラマキ
-            bid = ticker.best_bid
-            ask = ticker.best_ask
-            C = ohlcv.close[-1]
-            buy = flooring(C-25.5,51)
-            sell = ceiling(C+25.5,51)
-            lot = 0.01
-            for i in range(1):
-                bp = int(buy-i*51)
-                sp = int(sell+i*51)
-                strategy.order(str(bp), 'buy', qty=lot, limit=bp, minute_to_expire=1)
-                strategy.order(str(sp), 'sell', qty=lot, limit=sp, minute_to_expire=1)
+            # 遅延評価
+            dist = ohlcv.distribution_delay[-5:]
+            delay = sorted(dist)[int(len(dist)/2)]
 
-            # 損切り
-            for p in strategy.positions:
-                side, price, size = p['side'], p['price'], p['size']
-                if size>0.01:
-                    if side=='buy':
-                        pnl = bid - price
-                        if pnl<-102:
-                            strategy.order('C'+str(price), 'sell', qty=size)
-                    else:
-                        pnl = price-ask
-                        if pnl<-102:
-                            strategy.order('C'+str(price), 'buy', qty=size)
+            if delay<3:
+                # 51円値幅で指値バラマキ
+                mid = (ohlcv.close[-1]+ohlcv.high[-1]+ohlcv.low[-1])/3
+                spr = stdev(ohlcv.close)
+                lot = 0.01
+                rng = 51
+                buy = ((mid-spr/2)//rng)*rng
+                sell = ((mid+spr/2)//rng+1)*rng
+                strategy.order(f'{buy}', 'buy', qty=lot, limit=buy, minute_to_expire=1)
+                strategy.order(f'{sell}', 'sell', qty=lot, limit=sell, minute_to_expire=1)
+
+                # 利確・損切り
+                profit = rng
+                loss = -rng*0.5
+                for p in strategy.positions:
+                    side, price, size = p['side'], p['price'], p['size']
+                    if size>=lot:
+                        if side=='buy':
+                            pnl = mid-price
+                            if pnl<=loss or pnl>=profit:
+                                strategy.order(f'C{price}', 'sell', qty=size)
+                        else:
+                            pnl = price-mid
+                            if pnl<=loss or pnl>=profit:
+                                strategy.order(f'C{price}', 'buy', qty=size)
+            else:
+                strategy.cancel_order_all()
+                strategy.close_position()
         else:
             strategy.cancel_order_all()
             strategy.close_position()
@@ -73,6 +81,7 @@ if __name__ == "__main__":
     strategy = Strategy(fraction().loop, 0.5)
     strategy.settings.apiKey = settings.apiKey
     strategy.settings.secret = settings.secret
+    strategy.settings.max_ohlcv_size = 30
     strategy.settings.disable_rich_ohlcv = True
     strategy.risk.max_position_size = 0.1
     strategy.start()
