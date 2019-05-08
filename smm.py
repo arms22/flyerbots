066dabb493910ade7c1f4619322ec623f34e5e9a
arms22 <arms22@gmail.com>
2019-05-08 22:51:39 +0900

    smm.py update

--- smm.py
+++ smm.py
@@ -5,16 +5,16 @@ from flyerbots.strategy import Strategy
 from flyerbots.indicator import *
 
 no_trade_time_range = [
-    (time( 7,55), time( 8, 5)), # JST 16:55-17:05
-    (time( 8,55), time( 9, 5)), # JST 17:55-18:05
-    (time( 9,55), time(10, 5)), # JST 18:55-19:05
-    (time(10,55), time(11, 5)), # JST 19:55-20:05
-    (time(11,55), time(12, 5)), # JST 20:55-21:05
-    (time(12,55), time(13, 5)), # JST 21:55-22:05
-    (time(13,55), time(14, 5)), # JST 22:55-23:05
-    (time(14,55), time(15, 5)), # JST 23:55-00:05
-    (time(15,55), time(23, 5)), # JST 00:55-08:05
-    # (time(18,55), time(19, 55)), # JST 03:55-04:55 Bitflyerメンテナンスタイム
+    # (time( 7,55), time( 8, 5)), # JST 16:55-17:05
+    # (time( 8,55), time( 9, 5)), # JST 17:55-18:05
+    # (time( 9,55), time(10, 5)), # JST 18:55-19:05
+    # (time(10,55), time(11, 5)), # JST 19:55-20:05
+    # (time(11,55), time(12, 5)), # JST 20:55-21:05
+    # (time(12,55), time(13, 5)), # JST 21:55-22:05
+    # (time(13,55), time(14, 5)), # JST 22:55-23:05
+    # (time(14,55), time(15, 5)), # JST 23:55-00:05
+    # (time(15,55), time(23, 5)), # JST 00:55-08:05
+    (time(18,55), time(19, 55)), # JST 03:55-04:55 Bitflyerメンテナンスタイム
 ]
 
 def stdev(src):
@@ -55,38 +55,38 @@ class simple_market_maker:
 
             # 指値幅計算
             spr = max(stdev(ohlcv.close), 200)
-            # pairs = [(0.02, spr*0.75, '3', 14.5), (0.02, spr*0.5, '2', 9.5), (0.02, spr*0.25, '1', 4.5)]
             pairs = [(0.05, spr*0.5, '2', 9.5), (0.05, spr*0.25, '1', 4.5)]
-            maxsize = sum(p[0] for p in pairs)+0.025
+            maxsize = sum(p[0] for p in pairs)
             buymax = sellmax = strategy.position_size
             mid = (ohlcv.high[-1]+ohlcv.low[-1]+ohlcv.close[-1])/3
             z = zscore(ohlcv.volume_imbalance)
             ofs = z*9
 
-            if delay>2.5:
-                if strategy.position_size>=0.01:
-                    strategy.order('L close', 'sell', qty=0.01)
-                elif strategy.position_size<=-0.01:
-                    strategy.order('S close', 'buy', qty=0.01)
+            if delay>1.5:
+                if delay>3.5:
+                    if strategy.position_size>=0.01:
+                        strategy.order('Lc', 'sell', qty=0.01)
+                    elif strategy.position_size<=-0.01:
+                        strategy.order('Sc', 'buy', qty=0.01)
                 for _, _,suffix,_ in pairs:
                     strategy.cancel('L'+suffix)
                     strategy.cancel('S'+suffix)
             else:
-                strategy.cancel('L close')
-                strategy.cancel('S close')
                 for size, width, suffix, period in pairs:
+                    buyid = 'L'+suffix
+                    sellid = 'S'+suffix
                     if buymax+size <= maxsize:
-                        strategy.order('L'+suffix, 'buy', qty=size, limit=int(mid-width+ofs),
+                        strategy.order(buyid, 'buy', qty=size, limit=int(mid-width+ofs),
                             seconds_to_keep_order=period, minute_to_expire=1)
                         buymax += size
                     else:
-                        strategy.cancel('L'+suffix)
+                        strategy.cancel(buyid)
                     if sellmax-size >= -maxsize:
-                        strategy.order('S'+suffix, 'sell', qty=size, limit=int(mid+width+ofs),
+                        strategy.order(sellid, 'sell', qty=size, limit=int(mid+width+ofs),
                             seconds_to_keep_order=period, minute_to_expire=1)
                         sellmax -= size
                     else:
-                        strategy.cancel('S'+suffix)
+                        strategy.cancel(sellid)
         else:
             strategy.cancel_order_all()
             strategy.close_position()
