8883952250d95120c209810cbfe44c8c3bbd348d
arms22 <arms22@gmail.com>
2019-07-09 20:38:10 +0900

    update sfdbot.py

--- sfdbot.py
+++ sfdbot.py
@@ -1,21 +1,9 @@
 # -*- coding: utf-8 -*-
 from flyerbots.strategy import Strategy
-# from flyerbots.indicator import *
+from flyerbots.streaming import parse_exec_date
 from datetime import datetime, timedelta
 from math import ceil
 from collections import deque
-from statistics import mean, stdev
-
-def parse_exec_date(exec_date):
-    exec_date = exec_date.rstrip('Z')+'0000000'
-    return datetime(
-        int(exec_date[0:4]),
-        int(exec_date[5:7]),
-        int(exec_date[8:10]),
-        int(exec_date[11:13]),
-        int(exec_date[14:16]),
-        int(exec_date[17:19]),
-        int(exec_date[20:26]))
 
 class SFDBot:
 
@@ -25,22 +13,19 @@ class SFDBot:
 
     def setup(self, strategy):
         self.spot_ep = strategy.streaming.get_endpoint('BTC_JPY',['executions'])
-        self.spot_ep.wait_any(['executions'])
 
     def loop(self, executions, strategy, **other):
 
-        strategy.streaming.wait_any()
-        dt = datetime.utcnow()
-
-        execs = self.spot_ep.get_executions()
-        for e in execs:
+        spot_executions = self.spot_ep.get_executions()
+        if len(spot_executions):
+            e = spot_executions[-1]
             e['exec_date'] = parse_exec_date(e['exec_date'])
-        self.spot_q.extend(execs)
-
+            self.spot_q.append(e)
         if len(self.spot_q)<2:
             return
 
-        spot_available = len(execs)
+        dt = datetime.utcnow()
+        spot_available = len(spot_executions)
         spot = self.spot_q[-1]
         spot2 = self.spot_q[-2]
         spot_ltp = spot['price']
@@ -56,22 +41,10 @@ class SFDBot:
 
         sfdask = ceil(spot_ltp * 1.05)
         sfdbid = sfdask-1
-        # sfdask = ceil(ltp_max * 1.05)
-        # sfdbid = ceil(ltp_min * 1.05)-1
-
-        maxsize = 0.01
-        lot = 0.01
-        deltapos = strategy.position_size+0.02
-
-        # o = strategy.get_order('L')
-        # if o.status in ['open']:
-        #     if sfdbid<o.price:
-        #         strategy.cancel('L')
-
-        # o = strategy.get_order('S')
-        # if o.status in ['open']:
-        #     if sfdask>o.price:
-        #         strategy.cancel('S')
+        buymax = 0.00
+        sellmax = 0.04
+        lot = 0.02
+        deltapos = strategy.position_size+lot
 
         if spot_available:
             if spot_ltp > spot_ltp2:
@@ -81,12 +54,12 @@ class SFDBot:
 
         if strategy.api_token >= 4:
             if spot_past_time>0.37 and len(ltp_uniq)<=2:
-                if deltapos+lot<=maxsize:
+                if deltapos+lot<=buymax:
                     strategy.order('L', 'buy', qty=lot, limit=sfdbid, minute_to_expire=1)
-                if deltapos-lot>=-maxsize:
+                if deltapos-lot>=-sellmax:
                     strategy.order('S', 'sell', qty=lot, limit=sfdask, minute_to_expire=1)
 
-        logger.info(f'{spot_ltp} {spot_past_time:6.3f} {ltp_min} {ltp_max}')
+        # logger.info(f'{spot_ltp} {spot_past_time:6.3f} {ltp_min} {ltp_max}')
 
 if __name__ == "__main__":
     import settings
